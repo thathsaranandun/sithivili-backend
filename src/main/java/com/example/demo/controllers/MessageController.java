9af1cea81646d914b9752a486699d2becc850a32
Thathsara Nandun <w1697812@my.westminster.ac.uk>
2019-12-13 15:25:07 +0530

    Websocket configured
    Added websocket configuration for public chat

---
+++ src/main/java/com/example/demo/controllers/MessageController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import com.example.demo.websocketing.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
