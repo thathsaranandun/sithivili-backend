afc14e2a94e018046a3628bebd224e4cf4d0e44a
Thathsara Nandun <w1697812@my.westminster.ac.uk>
2020-01-04 12:22:27 +0530

    cloud

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,18 +1,22 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/")
 public class HomeController {
 
     /**
      * Test server status
      * @return
      */
-    @RequestMapping("")
-    public String home(){
-        return "Sithivili Server status - running";
+    @RequestMapping("/")
+    public ResponseEntity<?> home(){
+
+        return ResponseEntity.ok().build();
     }
+
 }
